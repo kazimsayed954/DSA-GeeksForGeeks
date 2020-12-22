3fa22bad367bb16db7094d4a333a6601dc92db45
kazimsayed954 <kazimsayed954@gmail.com>
2020-12-22 22:00:43 +0530

    added Mathematics/isPalindrome in java

---
+++ Mathematics/isPalindrome.java
@@ -0,0 +1,18 @@
+public class isPalindrome(){
+
+	public static boolean isPalin(int num){
+		int rev=0;
+		int temp=num;
+		while(temp!=0){
+			int lastDig=temp%10;
+			rev=rev*10 + lastDig;
+			temp=temp/10;
+		}
+
+			return (rev==num);
+	}
+
+	public static void main(String){
+		System.out.println(isPalin(7557))
+	}
+}
