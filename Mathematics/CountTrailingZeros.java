a49b1317132ad56fa58aab888197eb4835646e93
kazimsayed954 <kazimsayed954@gmail.com>
2020-12-23 20:35:54 +0530

    added mathematics/countTrailingZeros.java

--- Mathematics/CountTrailingZeros.java
+++ Mathematics/CountTrailingZeros.java
@@ -9,9 +9,25 @@ public class CountTrailingZeros{
 	    return res;
 	}
 
+	public static long nonConvinientMethodCountTrailingZeros(int num){
+	    long fact=1;
+	    int count=0;
+	    for(int i=2;i<=num;i++){
+	        fact*=i;
+	    }
+	    
+	    while(fact%10==0){
+	        count++;
+	        fact=fact/10;
+	    }
+	    return count;
+	}
+
+
 	public static void main(String[] args){
-		
-		System.out.println(countTrailing(5));	
+
+		System.out.println(countTrailing(5));
+		System.out.println(nonConvinientMethodCountTrailingZeros(10));	
 		
 	}
 
